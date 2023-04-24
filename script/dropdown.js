1075aa349237a8b195e358765ade0117574ba9f1
tenguri-jeon <110875363+tenguri-jeon@users.noreply.github.com>
2023-04-24 10:31:02 +0900

    ㄹㄹ

--- script/dropdown.js
+++ script/dropdown.js
@@ -1,27 +1,33 @@
-button();
-function button(params) {  
+accordion();
+function accordion(params) {  
     const $dropdownButton = document.getElementsByClassName('dropdown-button');
     const $submenuWrap = document.getElementsByClassName('submenu-wrap');
     const $tabMenu = document.getElementsByClassName('tab-menu');
+    const $tableBody = document.getElementsByClassName('query-adjust-list-tbody');
     
     for (let i = 0; i < $dropdownButton.length; i++) {
         
         $dropdownButton[i].addEventListener('click', (e)=>{
             // 버튼 클릭 시 버튼 모양이 바뀌는 이벤트와, 아코디언 메뉴가 나오는 이벤트 발생
+
+            // 밑으로 내리는 버튼 이벤트
             if ($dropdownButton[i].classList.contains('dropdown-icon-down')) {
                 $dropdownButton[i].classList.remove('dropdown-icon-down');
                 $dropdownButton[i].classList.add('dropdown-icon-up');
-                $submenuWrap[i].classList.add('display')
-                $tabMenu[i].classList.add('tab-menu-display')
+                $submenuWrap[i].classList.remove('display')
+                setTimeout(function() {
+                    $tabMenu[i].classList.remove('tab-menu-display')
+                }, 200)
+                $tableBody[0].style.height = '378px';
             }
+            // 다시 올리는 버튼 이벤트
             else{
                 $dropdownButton[i].classList.add('dropdown-icon-down');
                 $dropdownButton[i].classList.remove('dropdown-icon-up');
-                $submenuWrap[i].classList.remove('display')
+                $submenuWrap[i].classList.add('display')
                 $tabMenu[i].classList.add('tab-menu-display')
-                setTimeout(function() {
-                    $tabMenu[i].classList.remove('tab-menu-display')
-                }, 100)
+                $tabMenu[i].classList.add('tab-menu-display')
+                $tableBody[0].style.height = '178px';
             }
             // i번째 이외의 다른 버튼을 눌렀을 때, 버튼 모양을 바꿔주는 이벤트
             for (let j = 0; j < $dropdownButton.length; j++) {
